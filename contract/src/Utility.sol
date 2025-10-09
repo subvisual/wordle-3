5605aa5cef3ea1a9b9e443c33dd6e4eafa8d9caf
VitorLelis <vitorll909@gmail.com>
2025-10-09 17:33:47 +0100

    refactor(contract): Utility toLowerCase uppercase detection

--- contract/src/Utility.sol
+++ contract/src/Utility.sol
@@ -2,14 +2,13 @@
 pragma solidity ^0.8.13;
 
 library Utility {
-    //convert string to lowercase
+    // Convert string to lowercase
+    // If a character is Uppercase we add 32 to reach it's lowercase version (ASCII)
     function toLowerCase(string memory _str) public pure returns (string memory) {
         bytes memory bString = bytes(_str);
         bytes memory bLowerCase = new bytes(bString.length);
         for (uint j = 0; j < bString.length; j++) {
-            // Uppercase character...
-            if ((uint8(bString[j]) >= 65) && (uint8(bString[j]) <= 90)) {
-                // So we add 32 to make it lowercase
+            if ((bString[j] >= "A") && (bString[j] <= "Z")) {
                 bLowerCase[j] = bytes1(uint8(bString[j]) + 32);
             } else {
                 bLowerCase[j] = bString[j];
